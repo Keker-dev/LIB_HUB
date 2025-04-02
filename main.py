8f165be6897ad4600ae57328b1883050c39b734a
Ивлев Тимофей <timaiv112008@gmail.com>
2025-04-02 23:03:10 +0700

    Шаблон проекта 2.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import url_for, request, render_template, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'libhub_secret_key'
+
+
+@app.route("/")
+def main_page():
+    return render_template("base.html")
 
 
 def main():
