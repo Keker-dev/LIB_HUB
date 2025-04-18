dcd109b56d328fbd695364400f86d3b3c7481758
Ивлев Тимофей <timaiv112008@gmail.com>
2025-04-18 22:13:31 +0700

    Шрифт меняется. Релиз 50%.

--- forms/main_page.py
+++ forms/main_page.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField, SearchField
+from wtforms.validators import DataRequired, Optional
 
 
 class MainPageForm(FlaskForm):
     reg = SubmitField('Зарегистрироваться')
     log = SubmitField('Вход')
-    profile = SubmitField(' ')
+    profile = SubmitField(" ")
+    settings = SubmitField(" ")
     add_book = SubmitField("Добавить книгу")
-    search = StringField("Поиск книг")
+    search = SearchField("Поиск книг", validators=[Optional()])
     search_submit = SubmitField("")
